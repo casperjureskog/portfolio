598120cf1c3fd652ed42540bf9b6d3b57190edb2
casperjureskog <lcasper@mac.com>
2017-02-28 14:39:44 +0100

    add meny

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ page '/*.txt', layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
-
+set :haml, :format => :html5
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
